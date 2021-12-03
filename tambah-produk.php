d0886e4a83cd574f9c4c92884a3bda98a618703b
Asbudi Anugrah <asbudianugrahpatimasang@gmail.com>
2021-12-03 17:55:07 +0800

    module varian warna dgn foto
    js untuk onchange review sesua banyak data warna dan review foto.

--- tambah-produk.php
+++ tambah-produk.php
@@ -42,6 +42,38 @@ $usernamelogin = $row['username_user']
       rel="stylesheet"
       type="text/css"
     />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.13/css/select2.min.css" integrity="sha512-nMNlpuaDPrqlEls3IX/Q56H36qvBASwb3ipuo3MxeWbsQB1881ox0cRv7UPTgBlriqoynt35KjEwgGUeUXIPnw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    
+    
+    <script>
+      function namaWarna() {
+        var val = document.getElementById("js-example-basic-multiple");
+        var warna = "";
+        var n = 0;
+        for(var i = 0; i<val.options.length; i++){
+          if(val.options[i].selected === true){
+              n += 1; 
+              warna += '<tr>\
+                          <td scope="row">'+n+'</td>\
+                          <td>'+val.options[i].value+'</td>\
+                          <td>\
+                            <input type="file" onchange="previewWarna()" name="produk[]" id="upload_image'+n+'" accept="image/png,Image/jpeg" class="d-none">\
+                            <label for="upload_image'+n+'" style="cursor: pointer;" class="col-12">\
+                              <img id="warna'+n+'" style="max-width:100px;" src="assets/images/produk/img.jpg" alt="">\
+                            </label>\
+                          </td>\
+                        </tr>';
+          }
+        }
+        if(n != 0){
+          // document.getElementById("jum_color").value = n;
+          document.getElementById("d-warna").style.display = "block";
+          document.getElementById("s-warna").innerHTML = warna;
+        }else{
+          document.getElementById("d-warna").style.display = "none";
+        }
+      }
+    </script>
 
     <script type="text/javascript">
             function showTime() {
@@ -130,7 +162,7 @@ $usernamelogin = $row['username_user']
                           <img id="preview" style="display: block;margin-left: auto;margin-right: auto; max-width:180px;" src="assets/images/produk/img.jpg" alt="">
                         </label>
                         <label for="upload_image" style="cursor: pointer;" class="col-12 mt-2">
-                          <span class="text-info" style="display: block;text-align: center;font-size: 1em;">Ubah Foto</span>
+                          <span class="text-info" style="display: block;text-align: center;font-size: 1em;">Foto Depan</span>
                         </label>
                       </div>
                       <hr>
@@ -151,6 +183,7 @@ $usernamelogin = $row['username_user']
                         <div class="col-sm-9">
                           <select name="" id="" class="form-select">
                             <option value="">--PILIH kATEGORI--</option>
+                            <option value="1"></option>
                           </select>
                         </div>
                       </div>
@@ -163,9 +196,59 @@ $usernamelogin = $row['username_user']
                               Cantumkan min. 40 karakter agar semakin<br>menarik dan mudah ditemukan oleh<br>pembeli, terdiri dari jenis produk, merek,<br>dan keterangan seperti warna, bahan, atau tipe.
                             </p>
                         </div>
-                        <div class="col-sm">
+                        <div class="col-sm-9">
                           <textarea name="" id="" rows="4" class="form-control"></textarea>
                         </div>
+                        <div class="col-sm-3">
+                          <label for="" class="form-label">Jumlah Stok</label>
+                        </div>
+                        <div class="col-sm-9">
+                          <input type="number" name="stok" id="" class="form-control">
+                        </div>
+                        <div class="col-sm-3">
+                          <label for="" class="form-label">Varian Warna</label>
+                        </div>
+                        <div class="col-12 col-sm">
+                          <div class="input-group">
+                            <select class="form-select" id="js-example-basic-multiple" name="warna[]" multiple="multiple">
+                              <option value="Merah">Merah</option>
+                              <option value="Biru">Biru</option>
+                              <option value="Kuning">Kuning</option>
+                              <option value="Hijau">Hijau</option>
+                              <option value="Putih">Putih</option>
+                              <option value="Hitam">Hitam</option>
+                            </select>
+                            <button type="button" style="height: 2.1rem;" class="btn btn-outline-primary" onclick="namaWarna()">Submit</button>
+                          </div>
+                          <div style="display: none;" id="d-warna">
+                            <!-- <input type="number" id="jum_color" > -->
+                            <table class="table table-hover mt-2">
+                              <thead>
+                                <tr>
+                                  <th scope="col">#</th>
+                                  <th scope="col">Warna</th>
+                                  <th scope="col">Gambar</th>
+                                </tr>
+                              </thead>
+                              <tbody id="s-warna">
+                              </tbody>
+                            </table>
+                          </div>
+                        </div>
+                        <!-- <div class="col-sm-3">
+                          <label for="" class="form-label">Jumlah Warna</label>
+                        </div> -->
+                        <!-- <div class="col-sm-9">
+                          <input type="number" name="warna" onkeyup="namaWarna(this.value)" id="" class="form-control">
+                        </div>
+                        <div id="show">
+                        </div>
+                          <div class="col-sm-3">
+                            <label for="" class="form-label">Warna</label>
+                          </div>
+                          <div class="col-sm">
+                            
+                          </div> -->
                       </div>
                     </form>
                   </div>
@@ -270,7 +353,7 @@ $usernamelogin = $row['username_user']
     <script src="assets/libs/metismenu/metisMenu.min.js"></script>
     <script src="assets/libs/simplebar/simplebar.min.js"></script>
     <script src="assets/libs/node-waves/waves.min.js"></script>
-
+    
     <script src="assets/js/app.js"></script>
     <script>
         function previewImage() {
@@ -283,6 +366,64 @@ $usernamelogin = $row['username_user']
           };
         };
     </script>
+    <script>
+      function previewWarna(){
+        var img_name = "upload_image1";
+        var oFReader = new FileReader();
+        oFReader.readAsDataURL(document.getElementById(img_name).files[0]);
+          
+        oFReader.onload = function(oFREvent) {
+          var color = "warna1";
+          // document.getElementById("image-preview").style.display = "block";
+          document.getElementById(color).src = oFREvent.target.result;
+        };
+        var img_name = "upload_image2";
+        var oFReader = new FileReader();
+        oFReader.readAsDataURL(document.getElementById(img_name).files[0]);
+          
+        oFReader.onload = function(oFREvent) {
+          var color = "warna2";
+          // document.getElementById("image-preview").style.display = "block";
+          document.getElementById(color).src = oFREvent.target.result;
+        };
+        var img_name = "upload_image3";
+        var oFReader = new FileReader();
+        oFReader.readAsDataURL(document.getElementById(img_name).files[0]);
+          
+        oFReader.onload = function(oFREvent) {
+          var color = "warna3";
+          // document.getElementById("image-preview").style.display = "block";
+          document.getElementById(color).src = oFREvent.target.result;
+        };
+        var img_name = "upload_image4";
+        var oFReader = new FileReader();
+        oFReader.readAsDataURL(document.getElementById(img_name).files[0]);
+          
+        oFReader.onload = function(oFREvent) {
+          var color = "warna4";
+          // document.getElementById("image-preview").style.display = "block";
+          document.getElementById(color).src = oFREvent.target.result;
+        };
+        var img_name = "upload_image5";
+        var oFReader = new FileReader();
+        oFReader.readAsDataURL(document.getElementById(img_name).files[0]);
+          
+        oFReader.onload = function(oFREvent) {
+          var color = "warna5";
+          // document.getElementById("image-preview").style.display = "block";
+          document.getElementById(color).src = oFREvent.target.result;
+        };
+      }
+    </script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.13/js/select2.min.js"></script>
+    <script>
+      $(document).ready(function() {
+          $('#js-example-basic-multiple').select2({
+            placeholder: "--PILIH WARNA--"
+          });
+      });
+    </script>
+
   </body>
 </html>
 <?php mysqli_close($db->conn) ?>
